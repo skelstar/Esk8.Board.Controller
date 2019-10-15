4bbd955ea64db10c69834481f4c74c87572da0dc
Sean <skelstar@gmail.com>
2019-10-15 17:24:06 +1300

    Wired up nrf and it's compiling. Have to test with Server

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,36 @@
 #include <SPI.h>
 #include <NRF24L01Library.h>
 
+NRF24L01Lib nrf;
+
+#define SPI_CE        5 	//33    // white/purple
+#define SPI_CS        13	//26  // green
+
+RF24 radio(SPI_CE, SPI_CS);    // ce pin, cs pinRF24Network network();
+RF24Network network(radio); 
+
+
+void packet_cb( uint16_t from ) {
+	// lastAckFromBoard = millis();
+	// debug.print(COMMUNICATION, "Rx from Board: batt voltage %.1f, vescOnline: %d \n", 
+	// 	esk8.boardPacket.batteryVoltage, 
+	// 	esk8.boardPacket.vescOnline);
+	// updateDisplay();
+}
 
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+
+  SPI.begin();
+  radio.begin();
+  nrf.begin(&radio, &network, nrf.RF24_CLIENT, packet_cb);
+  radio.setAutoAck(true);
+
+  // WiFi.mode( WIFI_OFF );	// WIFI_MODE_NULL
+	// btStop();   // turn bluetooth module off
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  nrf.update();
 }
