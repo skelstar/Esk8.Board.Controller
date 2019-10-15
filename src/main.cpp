8604d76ccd72e35902ffc05491b8cf8194eff086
Sean Kelly <sean.kelly@xero.com>
2019-10-15 13:16:55 +1300

    Compiles. Doesn't create radio yet, just includes libraries.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#if (defined(__AVR__))
+    #include <avr\ pgmspace.h> 
+#else
+    #include <pgmspace.h> 
+#endif
+#include <Arduino.h>
+#include <SPI.h>
+#include <NRF24L01Library.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
