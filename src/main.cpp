e8aac64472f91fe5e95e3184772260e416b0a821
sean-kelly <skelstar@gmail.com>
2019-10-18 13:02:19 +1300

    RTOS hooked up. Queue hooked up.

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,18 @@ bool idFromBoardExpected(long id);
 #define NO_PACKET_RECEIVED_FROM_BOARD  -1
 long lastIdFromBoard = NO_PACKET_RECEIVED_FROM_BOARD;
 
+xQueueHandle xQueue;
+const TickType_t xTicksToWait = pdMS_TO_TICKS(100);
+// xQueueSendToBack(xQueue, &e, xTicksToWait);
+
+enum EventEnum
+{
+  EVENT_1,
+  EVENT_2,
+  EVENT_3
+} event;
+
+//------------------------------------------------------------------
 void sendToServer() {
 
   if (!idFromBoardExpected(nrf24.boardPacket.id)) {
@@ -26,7 +38,7 @@ void sendToServer() {
   lastIdFromBoard = nrf24.boardPacket.id;
   bool success = nrf24.sendPacket(nrf24.RF24_SERVER);
   if (success) {
-    Serial.printf("Replied to %l OK (with %l)\n", 
+    Serial.printf("Replied to %u OK (with %u)\n", 
       nrf24.boardPacket.id, 
       nrf24.controllerPacket.id);
   }
@@ -45,6 +57,24 @@ void packet_cb( uint16_t from ) {
   sendToServer();
 }
 //------------------------------------------------------------------
+#define OTHER_CORE 0
+
+void coreTask(void *pvParameters) {
+
+  Serial.printf("Task running on core %d\n", xPortGetCoreID());
+
+  long other_now = 0;
+  while(true){
+    if (millis() - other_now > 2000) {
+      other_now = millis();
+      EventEnum e = EVENT_3;
+      xQueueSendToBack(xQueue, &e, xTicksToWait);
+    }
+
+  }
+  vTaskDelete( NULL );
+}
+
 void setup() {
 
   Serial.begin(115200);
@@ -54,16 +84,41 @@ void setup() {
   nrf24.begin(&radio, &network, nrf24.RF24_CLIENT, packet_cb);
   radio.setAutoAck(true);
 
+  xTaskCreatePinnedToCore(coreTask, "coreTask", 10000, NULL, /*priority*/ 0, NULL, OTHER_CORE);
+
+  xQueue = xQueueCreate(1, sizeof(EventEnum));
+
+  Serial.printf("Loop running on core %d\n", xPortGetCoreID());
+
   // WiFi.mode( WIFI_OFF );	// WIFI_MODE_NULL
 	// btStop();   // turn bluetooth module off
 }
 //------------------------------------------------------------------
 
 long now = 0;
+BaseType_t xStatus;
 
 void loop() {
 
   nrf24.update();
+
+  EventEnum e;
+  xStatus = xQueueReceive( xQueue, &e, xTicksToWait );
+  if(xStatus == pdPASS){
+    switch (e) {
+      case EVENT_1:
+        Serial.printf("Event %d\n", e);
+        break;  
+      case EVENT_2:
+        Serial.printf("Event %d\n", e);
+        break;
+      case EVENT_3:
+        Serial.printf("Event %d\n", e);
+        break;
+      default:
+        Serial.printf("Unhandled event code: %d \n", e);
+    }
+  } 
 
 }
 //------------------------------------------------------------------
