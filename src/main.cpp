01fccefba717a0fcf95a3970fb94df6854255f0f
Sean <skelstar@gmail.com>
2019-10-19 15:58:40 +1300

    Added "missed: %d" to screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <NRF24L01Library.h>
 #include <Wire.h>
 #include "SSD1306.h"
-
+//------------------------------------------------------------------
 NRF24L01Lib nrf24;
 
 #define SPI_CE        33    // white/purple
@@ -17,18 +17,34 @@ bool idFromBoardExpected(long id);
 
 #define NO_PACKET_RECEIVED_FROM_BOARD  -1
 long lastIdFromBoard = NO_PACKET_RECEIVED_FROM_BOARD;
+uint8_t missedPacketsCounter = 0;
+
+#include "utils.h"
+//------------------------------------------------------------------
+void updateDisplayWithMissedPacketCount() {
+  u8g2.clearBuffer();
+  u8g2.setFontPosTop();
+  char buffx[16];
+  sprintf(buffx, "Missed: %d", missedPacketsCounter);
+  u8g2.setFont(FONT_SIZE_MED); // full
+  int width = u8g2.getStrWidth(buffx);
+  u8g2.drawStr(LCD_WIDTH/2-width/2, LCD_HEIGHT/2-FONT_SIZE_MED_LINE_HEIGHT/2, buffx);
+  u8g2.sendBuffer();
+}
 
 void sendToServer() {
 
   if (!idFromBoardExpected(nrf24.boardPacket.id)) {
-    Serial.printf("Id from board not expected: %l\n", nrf24.controllerPacket.id);
+    Serial.printf("Id '%u' from board not expected\n", nrf24.controllerPacket.id);
+    missedPacketsCounter++;
+    updateDisplayWithMissedPacketCount();
   }
   
   nrf24.controllerPacket.id = nrf24.boardPacket.id;
   lastIdFromBoard = nrf24.boardPacket.id;
   bool success = nrf24.sendPacket(nrf24.RF24_SERVER);
   if (success) {
-    Serial.printf("Replied to %l OK (with %l)\n", 
+    Serial.printf("Replied to %u OK (with %u)\n", 
       nrf24.boardPacket.id, 
       nrf24.controllerPacket.id);
   }
@@ -37,60 +53,10 @@ void sendToServer() {
   }
 }
 
-bool idFromBoardExpected(long id) {
-  return lastIdFromBoard == NO_PACKET_RECEIVED_FROM_BOARD 
-      || id == lastIdFromBoard + 1;
-}
-
 void packet_cb( uint16_t from ) {
   // Serial.printf("packet_cb(%d)\n", from);
   sendToServer();
 }
-
-// void scanFori2cDevice() {
-//   byte error, address;
-//   int nDevices;
- 
-//   Serial.println("Scanning...");
- 
-//   nDevices = 0;
-//   for(address = 1; address < 127; address++ )
-//   {
-//     // The i2c_scanner uses the return value of
-//     // the Write.endTransmisstion to see if
-//     // a device did acknowledge to the address.
-//     Wire.beginTransmission(address);
-//     error = Wire.endTransmission();
- 
-//     if (error == 0)
-//     {
-//       Serial.print("I2C device found at address 0x");
-//       if (address<16)
-//         Serial.print("0");
-//       Serial.print(address,HEX);
-//       Serial.println("  !");
- 
-//       nDevices++;
-//     }
-//     else if (error==4)
-//     {
-//       Serial.print("Unknown error at address 0x");
-//       if (address<16)
-//         Serial.print("0");
-//       Serial.println(address,HEX);
-//     }    
-//   }
-//   if (nDevices == 0)
-//     Serial.println("No I2C devices found\n");
-//   else
-//     Serial.println("done\n");
- 
-//   delay(5000);           // wait 5 seconds for next scan
-// }
-
-
-
-
 //------------------------------------------------------------------
 void setup() {
 
@@ -108,6 +74,8 @@ void setup() {
 
   // WiFi.mode( WIFI_OFF );	// WIFI_MODE_NULL
 	// btStop();   // turn bluetooth module off
+
+  updateDisplayWithMissedPacketCount();
 }
 //------------------------------------------------------------------
 
