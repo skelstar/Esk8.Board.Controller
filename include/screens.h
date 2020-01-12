facae99f965b924a74cc42363998d26fe502e101
sean-kelly <skelstar@gmail.com>
2020-01-13 06:53:48 +1300

    Tidied up USE_DEADMAN_SWITCH

--- include/screens.h
+++ include/screens.h
@@ -26,17 +26,19 @@ void screen_not_moving(uint8_t trigger_state)
   sprintf(buff2, "trig: %d", trigger_state);
   
   u8g2.clearBuffer();
+  // line 1
   lcd_message(/*line#*/ 1, "Stopped");
+  //line 2
+  char buff3[12];
+#ifdef USE_DEADMAN_SWITCH  
   switch (trigger_state)
   {
     case 0: lcd_message(/*line*/ 2, "trig: go!"); break;
     case 1: lcd_message(/*line*/ 2, "trig: wait"); break;
     case 2: lcd_message(/*line*/ 2, "trig: hold"); break;
   }
-  
-  char buff3[12];
+#endif
   sprintf(buff3, "bd rsts: %d", board_first_packet_count);
   lcd_message(/*line#*/ 3, &buff3[0]);
-  
   u8g2.sendBuffer();
 }
