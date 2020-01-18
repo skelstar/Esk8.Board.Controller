002e50251898e7daaebcac2d4c8f6e310ee41823
Sean <skelstar@gmail.com>
2020-01-18 21:45:04 +1300

    sending REQUEST and getting RESPONSE. fsm FTW!

--- src/comms_with_fsm_2/comms_with_fsm_2.cpp
+++ src/comms_with_fsm_2/comms_with_fsm_2.cpp
@@ -19,6 +19,8 @@
 
 VescData board_packet;
 
+ControllerData controller_packet;
+
 NRF24L01Lib nrf24;
 
 RF24 radio(SPI_CE, SPI_CS);
@@ -27,17 +29,33 @@ RF24Network network(radio);
 #define NUM_RETRIES 5
 
 elapsedMillis since_sent_to_board;
+
+#include <comms_fsm_2.h>
 //------------------------------------------------------------------
 
 void packet_available_cb(uint16_t from_id, uint8_t type)
 {
-  ControllerData board_packet;
 
-  uint8_t buff[sizeof(ControllerData)];
-  nrf24.read_into(buff, sizeof(ControllerData));
-  memcpy(&board_packet, &buff, sizeof(ControllerData));
+  uint8_t buff[sizeof(VescData)];
+  nrf24.read_into(buff, sizeof(VescData));
+  memcpy(&board_packet, &buff, sizeof(VescData));
 
   DEBUGVAL(from_id, board_packet.id, since_sent_to_board);
+}
+
+void send_to_board()
+{
+    DEBUGVAL("sending...", controller_packet.id);
+
+    uint8_t bs[sizeof(ControllerData)];
+    memcpy(bs, &controller_packet, sizeof(ControllerData));
+
+    uint8_t retries = nrf24.send_with_retries(/*to*/COMMS_BOARD, /*type*/ 0, bs, sizeof(ControllerData), NUM_RETRIES);
+    if (retries > 0)
+    {
+      DEBUGVAL(retries);
+    }
+    controller_packet.id++;
 }
 
 void setup()
@@ -45,6 +63,8 @@ void setup()
   Serial.begin(115200);
 
   nrf24.begin(&radio, &network, COMMS_CONTROLLER, packet_available_cb);
+
+  add_comms_fsm_transitions();
 
   DEBUG("Ready to rx from board...");
 }
@@ -54,18 +74,10 @@ void loop()
   if (since_sent_to_board > 1000)
   {
     since_sent_to_board = 0;
-    DEBUG("sending..");
-
-    uint8_t bs[sizeof(VescData)];
-    memcpy(bs, &board_packet, sizeof(VescData));
-
-    uint8_t retries = nrf24.send_with_retries(/*to*/COMMS_BOARD, /*type*/ 0, bs, sizeof(VescData), NUM_RETRIES);
-    if (retries > 0)
-    {
-      DEBUGVAL(retries);
-    }
-    board_packet.id++;
+    send_to_board();
   }
 
   nrf24.update();
+
+  comms_fsm.run_machine();
 }
