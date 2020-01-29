3f157e991e8708f77a79a7f6bfaf58f9705c6ec2
sean-kelly <skelstar@gmail.com>
2020-01-30 07:09:19 +1300

    Got timer working in rtos_play. Investigated and documented task semaphore behaviors.

--- src/rtos_play/rtos_play.cpp
+++ src/rtos_play/rtos_play.cpp
@@ -8,6 +8,7 @@ xQueueHandle xQueue2;
 SemaphoreHandle_t xTheSemaphore;
 // SemaphoreHandle_t xSemaphore2;
 
+xTimerHandle xTimer1;
 
 //---------------------------------------------------------------------
 void send_to_queue1(uint8_t ev)
@@ -40,30 +41,45 @@ uint8_t read_from_queue2(TickType_t ticks)
   return 0;
 }
 //---------------------------------------------------------------------
+bool waiting_for_semaphore(SemaphoreHandle_t sempahore, TickType_t ticks)
+{
+  /* 
+  tried to take the semaphore for x ticks then returns the result. If it can't
+  take the semaphore then it returns false, true otherwise
+  */
+  return sempahore == NULL || xSemaphoreTake(sempahore, ticks) != pdTRUE;
+}
+//---------------------------------------------------------------------
 void task_1(void *pvParameters)
 {
+  elapsedMillis since_took_semaphore_task_1;
   Serial.printf("task_1 running on core %d\n", xPortGetCoreID());
-  unsigned long task_1_now = millis();
 
   while (true)
   {
-    if (millis() - task_1_now > 10000)
+    if (since_took_semaphore_task_1 > 10000)
     {
-      task_1_now = millis();
+      since_took_semaphore_task_1 = 0;
 
-      if (xTheSemaphore != NULL && xSemaphoreTake(xTheSemaphore, (TickType_t) 10) == pdTRUE)
+      while (waiting_for_semaphore(xTheSemaphore, 200))
       {
-        Serial.printf("--> task_1 took semaphore\n");
-        vTaskDelay(4000);
-        Serial.printf("<-- task_1 giving semaphore after %lums\n", millis() - task_1_now);
-        xSemaphoreGive(xTheSemaphore);
-      }
-      else {
         Serial.printf(" XXX --> task_1 blocked!\n");
       }
-      // read_from_queue1(2000);
+
+      Serial.printf("--> task_1 took semaphore\n");
+      elapsedMillis since_started_task_0_loop = 0;
+      while (since_started_task_0_loop < 4000)
+      {
+        /*
+        if you remove the taskDelay, then other threads will not be switched to
+        by the task scheduler until the vTaskDelay(10) below
+        */
+        vTaskDelay(1);
+      }
+      xSemaphoreGive(xTheSemaphore);
+      Serial.printf("<-- task_1 giving semaphore after %lums\n", (unsigned long)since_started_task_0_loop);
     }
-    vTaskDelay(10);
+    vTaskDelay(10);  // <--- task exits here (I think)
   }
   vTaskDelete(NULL);
 }
@@ -72,24 +88,31 @@ void task_2(void *pvParameters)
 {
   Serial.printf("task_2 running on core %d\n", xPortGetCoreID());
 
-  unsigned long task_2_now = 0;
+  elapsedMillis task_2_try_take_semaphore = 0;
 
   while (true)
   {
-    if (millis() - task_2_now > 3000)
+    if (task_2_try_take_semaphore > 3000)
     {
-      task_2_now = millis();
+      task_2_try_take_semaphore = 0;
 
-      if (xTheSemaphore != NULL && xSemaphoreTake(xTheSemaphore, (TickType_t) 10) == pdTRUE)
+      while (waiting_for_semaphore(xTheSemaphore, /*ticks*/200))
       {
-        Serial.printf("==> task_2 took semaphore\n");
-        vTaskDelay(2000);
-        Serial.printf("<== task_2 giving semaphore after %lums\n", millis() - task_2_now);
-        xSemaphoreGive(xTheSemaphore);
-      }
-      else {
         Serial.printf(" XXX --> task_2 blocked!\n");
       }
+
+      Serial.printf("--> task_2 took semaphore\n");
+      elapsedMillis since_started_task_2_loop = 0;
+      while (since_started_task_2_loop < 2000)
+      {
+        /*
+        if you remove the taskDelay, then other threads will not be switched to
+        by the task scheduler until the vTaskDelay(10) below
+        */
+        vTaskDelay(1);
+      }
+      Serial.printf("<== task_2 giving semaphore after %lums\n", (unsigned long)since_started_task_2_loop);
+      xSemaphoreGive(xTheSemaphore);
     }
     vTaskDelay(10);
   }
@@ -109,7 +132,7 @@ void task_queue_seeder(void *pvParameters)
     if (since_seeded_queue > 5000)
     {
       since_seeded_queue = 0;
-      Serial.printf("--------------------------\nSeeding queue: %d\n", seed_val);
+      // Serial.printf("--------------------------\nSeeding queue: %d\n", seed_val);
       send_to_queue1(seed_val);
       seed_val++;
     }
@@ -130,13 +153,13 @@ void task_queue_peeker(void *pvParameters)
     if (since_peeked_queue > 1000)
     {
       since_peeked_queue = 0;
-      if (xQueuePeek(xQueue1, &peeked_val, (TickType_t) 10))
+      if (xQueuePeek(xQueue1, &peeked_val, (TickType_t)10))
       {
-        Serial.printf("Peeking queue: %d\n", peeked_val);
+        // Serial.printf("Peeking queue: %d\n", peeked_val);
       }
-      else 
+      else
       {
-        Serial.printf("FAILED to peek queue\n");
+        // Serial.printf("FAILED to peek queue\n");
       }
     }
     vTaskDelay(10);
@@ -157,7 +180,7 @@ void task_queue_reader(void *pvParameters)
     {
       since_read_queue = 0;
       read_val = read_from_queue1(10);
-      Serial.printf("-------> read val: %d\n", read_val);
+      // Serial.printf("-------> read val: %d\n", read_val);
     }
     vTaskDelay(10);
   }
@@ -165,22 +188,34 @@ void task_queue_reader(void *pvParameters)
 }
 //---------------------------------------------------------------------
 
-void vTimer1_cb( TimerHandle_t xTimer )
+elapsedMillis since_last_timer1 = 0;
+
+void vTimer1_cb(TimerHandle_t xTimer)
 {
-  Serial.printf("Timer1!\n");
+  if (since_last_timer1 > xTimerGetPeriod(xTimer) + 10)
+  {
+    Serial.printf("Timer1! %lu\n", (unsigned long)since_last_timer1);
+  }
+  since_last_timer1 = 0;
 }
 //---------------------------------------------------------------------
 
 void setup()
 {
   Serial.begin(115200);
-  // xTaskCreatePinnedToCore(task_1, "task_1", 4092, NULL, /*priority*/ 4, NULL, 1);
-  // xTaskCreatePinnedToCore(task_2, "task_2", 4092, NULL, /*priority*/ 3, NULL, 1);
+  xTaskCreatePinnedToCore(task_1, "task_1", 4092, NULL, /*priority*/ 4, NULL, 1);
+  xTaskCreatePinnedToCore(task_2, "task_2", 4092, NULL, /*priority*/ 3, NULL, 1);
   xTaskCreatePinnedToCore(task_queue_seeder, "task_queue_seeder", 4092, NULL, /*priority*/ 2, NULL, 1);
   xTaskCreatePinnedToCore(task_queue_peeker, "task_queue_peeker", 4092, NULL, /*priority*/ 2, NULL, 1);
   xTaskCreatePinnedToCore(task_queue_reader, "task_queue_reader", 4092, NULL, /*priority*/ 2, NULL, 1);
 
-  xTimerHandle xTimer1 = xTimerCreate("xTimer1", 100, pdTRUE, (void*) 0, vTimer1_cb);
+  xTimer1 = xTimerCreate(
+      "xTimer1",
+      /*ticks*/ 500,
+      pdTRUE,
+      (void *)0,
+      vTimer1_cb);
+
   if (xTimer1 != NULL)
   {
     if (xTimerStart(xTimer1, 0) != pdPASS)
@@ -194,8 +229,6 @@ void setup()
 
   xTheSemaphore = xSemaphoreCreateMutex();
   // xSemaphore2 = xSemaphoreCreateMutex();
-
-  vTaskStartScheduler();
 }
 
 void loop()
