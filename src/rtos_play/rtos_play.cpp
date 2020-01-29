3654303b6bf94f0495f88f814a81155e3bd157e0
sean-kelly <skelstar@gmail.com>
2020-01-30 07:33:32 +1300

    Passing in config object to rtos tasks.

--- src/rtos_play/rtos_play.cpp
+++ src/rtos_play/rtos_play.cpp
@@ -1,12 +1,32 @@
 #include <Arduino.h>
 
 #include <elapsedMillis.h>
+//---------------------------------------------------------------------
+class TaskConfig
+{
+  public:
+
+    TaskConfig(unsigned long f, unsigned long b, unsigned long w)
+    {
+      freq = f;
+      blocking_period = b;
+      wait_ticks = w;
+    }
+
+    unsigned long freq;
+    unsigned long blocking_period;
+    unsigned long wait_ticks;
+};
+//---------------------------------------------------------------------
 
 xQueueHandle xQueue1;
 xQueueHandle xQueue2;
 
 SemaphoreHandle_t xTheSemaphore;
 // SemaphoreHandle_t xSemaphore2;
+
+TaskConfig task1_config = TaskConfig(1000, 100, 100);
+TaskConfig task2_config = TaskConfig(3000, 100, 100);
 
 xTimerHandle xTimer1;
 
@@ -54,21 +74,22 @@ void task_1(void *pvParameters)
 {
   elapsedMillis since_took_semaphore_task_1;
   Serial.printf("task_1 running on core %d\n", xPortGetCoreID());
+  TaskConfig config = *((TaskConfig*) pvParameters);
 
   while (true)
   {
-    if (since_took_semaphore_task_1 > 10000)
+    if (since_took_semaphore_task_1 > config.freq)
     {
       since_took_semaphore_task_1 = 0;
 
-      while (waiting_for_semaphore(xTheSemaphore, 200))
+      while (waiting_for_semaphore(xTheSemaphore, config.wait_ticks))
       {
         Serial.printf(" XXX --> task_1 blocked!\n");
       }
 
       Serial.printf("--> task_1 took semaphore\n");
       elapsedMillis since_started_task_0_loop = 0;
-      while (since_started_task_0_loop < 4000)
+      while (since_started_task_0_loop < config.blocking_period)
       {
         /*
         if you remove the taskDelay, then other threads will not be switched to
@@ -89,21 +110,22 @@ void task_2(void *pvParameters)
   Serial.printf("task_2 running on core %d\n", xPortGetCoreID());
 
   elapsedMillis task_2_try_take_semaphore = 0;
+  TaskConfig config = *((TaskConfig*) pvParameters);
 
   while (true)
   {
-    if (task_2_try_take_semaphore > 3000)
+    if (task_2_try_take_semaphore > config.freq)
     {
       task_2_try_take_semaphore = 0;
 
-      while (waiting_for_semaphore(xTheSemaphore, /*ticks*/200))
+      while (waiting_for_semaphore(xTheSemaphore, /*ticks*/config.wait_ticks))
       {
         Serial.printf(" XXX --> task_2 blocked!\n");
       }
 
       Serial.printf("--> task_2 took semaphore\n");
       elapsedMillis since_started_task_2_loop = 0;
-      while (since_started_task_2_loop < 2000)
+      while (since_started_task_2_loop < config.blocking_period)
       {
         /*
         if you remove the taskDelay, then other threads will not be switched to
@@ -203,8 +225,9 @@ void vTimer1_cb(TimerHandle_t xTimer)
 void setup()
 {
   Serial.begin(115200);
-  xTaskCreatePinnedToCore(task_1, "task_1", 4092, NULL, /*priority*/ 4, NULL, 1);
-  xTaskCreatePinnedToCore(task_2, "task_2", 4092, NULL, /*priority*/ 3, NULL, 1);
+
+  xTaskCreatePinnedToCore(task_1, "task_1", 4092, (void *) &task1_config, /*priority*/ 4, NULL, 1);
+  xTaskCreatePinnedToCore(task_2, "task_2", 4092, (void *) &task2_config, /*priority*/ 3, NULL, 1);
   xTaskCreatePinnedToCore(task_queue_seeder, "task_queue_seeder", 4092, NULL, /*priority*/ 2, NULL, 1);
   xTaskCreatePinnedToCore(task_queue_peeker, "task_queue_peeker", 4092, NULL, /*priority*/ 2, NULL, 1);
   xTaskCreatePinnedToCore(task_queue_reader, "task_queue_reader", 4092, NULL, /*priority*/ 2, NULL, 1);
@@ -228,7 +251,6 @@ void setup()
   xQueue2 = xQueueCreate(1, sizeof(uint8_t));
 
   xTheSemaphore = xSemaphoreCreateMutex();
-  // xSemaphore2 = xSemaphoreCreateMutex();
 }
 
 void loop()
