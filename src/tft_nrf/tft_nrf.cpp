1c4b1ebf0b705e2ad7e2790eb93e9289edd7811c
Sean <skelstar@gmail.com>
2020-02-05 18:41:29 +1300

    Not working yet

--- src/tft_nrf/tft_nrf.cpp
+++ src/tft_nrf/tft_nrf.cpp
@@ -7,78 +7,70 @@
 #include <Arduino.h>
 #include <elapsedMillis.h>
 
+#include <VescData.h>
 #include <SPI.h>
-#include <RF24Network.h>
-#include <NRF24L01Lib.h>
-// #include <TFT_eSPI.h>
-#include <Adafruit_GFX.h>
-// #include <Arduino_ST7789.h>
-#include <Adafruit_ST7789.h>
-
+#include <NRFLite.h>
+#include <TFT_eSPI.h>
 
 // #define TFT_MOSI  19   // for hardware SPI data pin (all of available pins)
 // #define TFT_SCLK  18   // for hardware SPI sclk pin (all of available pins)
-#define TFT_CS    5 // only for displays with CS pin
-#define TFT_DC    16
-#define TFT_RST   23 
+#define TFT_CS 5 // only for displays with CS pin
+#define TFT_DC 16
+#define TFT_RST 23
 
 #define NRF_MOSI 23 // blue?
 #define NRF_MISO 19 // orange?
 #define NRF_CLK 18  // yellow
-#define NRF_CS 12 // green
-#define NRF_CE 16 // same as tft 15 // white
+#define NRF_CS 33   // green
+#define NRF_CE 26   // same as tft 15 // white
 
 #define COMMS_BOARD 00
 #define COMMS_CONTROLLER 01
 
-const uint16_t  Display_Color_Black        = 0x0000;
-const uint16_t  Display_Color_Blue         = 0x001F;
-const uint16_t  Display_Color_Red          = 0xF800;
-const uint16_t  Display_Color_Green        = 0x07E0;
-const uint16_t  Display_Color_Cyan         = 0x07FF;
-const uint16_t  Display_Color_Magenta      = 0xF81F;
-const uint16_t  Display_Color_Yellow       = 0xFFE0;
-const uint16_t  Display_Color_White        = 0xFFFF;
+const uint16_t Display_Color_Black = 0x0000;
+const uint16_t Display_Color_Blue = 0x001F;
+const uint16_t Display_Color_Red = 0xF800;
+const uint16_t Display_Color_Green = 0x07E0;
+const uint16_t Display_Color_Cyan = 0x07FF;
+const uint16_t Display_Color_Magenta = 0xF81F;
+const uint16_t Display_Color_Yellow = 0xFFE0;
+const uint16_t Display_Color_White = 0xFFFF;
 //------------------------------------------------------------------
 
-NRF24L01Lib nrf24;
+// NRF24L01Lib nrf24;
 
-RF24 radio(NRF_CE, NRF_CS);
-RF24Network network(radio);
+// RF24 radio(NRF_CE, NRF_CS);
+// RF24Network network(radio);
 
-void packet_available_cb(uint16_t from_id, uint8_t type)
-{
-}
+NRFLite _radio(Serial);
 
-// TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
-// Arduino_ST7789 tft = Arduino_ST7789(TFT_DC, TFT_RST, TFT_CS);
+const static uint8_t RADIO_ID = 0;             // Our radio's id.
+const static uint8_t DESTINATION_RADIO_ID = 1; // Id of the radio we will transmit to.
 
-// #if (SPI_INTERFACES_COUNT == 1)
-  SPIClass* spi = &SPI;
-// #else
-//   SPIClass* spi = &SPI1;
-// #endif
-
-Adafruit_ST7789 tft = Adafruit_ST7789(spi, TFT_CS, TFT_DC, TFT_RST);
+TFT_eSPI tft = TFT_eSPI(135, 240);
 
 elapsedMillis since_sent_to_board;
 
 //------------------------------------------------------------
 
-#include <tft.h>
-
 void init_tft()
 {
-  DEBUG("setup_tft()");
-  setup_tft();
+  DEBUG("-----------------------\nsetup_tft()\n-----------------------");
+
+  tft.init();
+  tft.setRotation(1);       // 0 is portrait
+  tft.fillScreen(TFT_BLUE); // Clear screen
+  tft.setTextSize(3);
+  tft.drawString("ready", 20, 20);
 }
 
 //------------------------------------------------------------
 
 void init_nrf()
 {
-  DEBUG("setup_nrf()");
-  nrf24.begin(&radio, &network, COMMS_CONTROLLER, packet_available_cb);
+  DEBUG("-----------------------\nsetup_nrf()\n-----------------------");
+  _radio.init(RADIO_ID, NRF_MISO, NRF_MOSI, NRF_CLK, NRF_CE, NRF_CS, NRFLite::BITRATE250KBPS, 100);
+  _radio.printDetails();
 }
 
 //------------------------------------------------------------------
@@ -87,15 +79,23 @@ void setup()
   Serial.begin(115200);
   delay(100);
 
-  
   init_tft();
-  delay(100);
+  delay(1000);
   init_nrf();
-
 }
 //------------------------------------------------------------------
+
+VescData vesc_data;
+
 void loop()
 {
+  while (_radio.hasData())
+  {
+    _radio.readData(&vesc_data);
+
+    DEBUGVAL("packet!", vesc_data.id);
+  }
+
   vTaskDelay(100);
 }
 //------------------------------------------------------------------
