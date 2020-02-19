3c08a4d47a004cca4b8c279251dbc91165be38d7
Sean <skelstar@gmail.com>
2020-02-20 05:15:20 +1300

    Display not wokring right after nrf sending packet.

--- src/nrf_tft_spi_sharing/nrf_tft_spi_sharing.cpp
+++ src/nrf_tft_spi_sharing/nrf_tft_spi_sharing.cpp
@@ -10,42 +10,11 @@
 #include <RF24Network.h>
 #include <NRF24L01Lib.h>
 
-// #define TFT_MOSI 19
-// #define TFT_SCLK 18
-// #define TFT_CS 5
-// #define TFT_DC 16
-// #define TFT_RST 23
-// #define TFT_BL 4 // Display backlight control pin
-
-#define DEFAULT_DISP_TYPE DISP_TYPE_ST7789V
-#define DEFAULT_TFT_DISPLAY_WIDTH 240
-#define DEFAULT_TFT_DISPLAY_HEIGHT 320
-#define DISP_COLOR_BITS_24 0x66
-#define DEFAULT_GAMMA_CURVE 0
-#define DEFAULT_SPI_CLOCK 20000000
-#define TFT_INVERT_ROTATION 0
-#define TFT_INVERT_ROTATION1 1
-#define TFT_RGB_BGR 0x00
-
-#define USE_TOUCH TOUCH_TYPE_NONE
-
-// #define PIN_NUM_MISO 0  // SPI MISO
-// #define PIN_NUM_MOSI 19 // SPI MOSI
-// #define PIN_NUM_CLK 18  // SPI CLOCK pin
-// #define PIN_NUM_CS 5    // Display CS pin
-// #define PIN_NUM_DC 16   // Display command/data pin
-// #define PIN_NUM_TCS 0   // Touch screen CS pin
-
-// #define PIN_NUM_RST 23 // GPIO used for RESET control
-// #define PIN_NUM_BCKL 4 // GPIO used for backlight control
-// #define PIN_BCKL_ON 1  // GPIO value for backlight ON
-// #define PIN_BCKL_OFF 0 // GPIO value for backlight OFF
-
 #include <TFT_eSPI.h>
 
-#define NRF_MISO 23 // Orange
-#define NRF_MOSI 19 // Blue
-#define NRF_CLK 18  // Yellow
+// #define NRF_MISO 23 // Orange
+// #define NRF_MOSI 19 // Blue
+// #define NRF_CLK 18  // Yellow
 #define NRF_CE 26
 #define NRF_CS 33
 
@@ -55,6 +24,7 @@
 //------------------------------------------------------------------
 
 VescData board_packet;
+ControllerData controller_packet;
 
 NRF24L01Lib nrf24;
 
@@ -76,13 +46,13 @@ TFT_eSPI tft = TFT_eSPI(LCD_HEIGHT, LCD_WIDTH); // Invoke custom library
 
 void packet_available_cb(uint16_t from_id, uint8_t type)
 {
-  ControllerData board_packet;
+  ControllerData controller_packet;
 
   uint8_t buff[sizeof(ControllerData)];
   nrf24.read_into(buff, sizeof(ControllerData));
-  memcpy(&board_packet, &buff, sizeof(ControllerData));
+  memcpy(&controller_packet, &buff, sizeof(ControllerData));
 
-  DEBUGVAL(from_id, board_packet.id, since_sent_to_board);
+  DEBUGVAL(from_id, controller_packet.id, since_sent_to_board);
 }
 //------------------------------------------------------------------
 
@@ -100,12 +70,7 @@ void init_tft()
 
 void init_nrf()
 {
-  radio.begin();
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_250KBPS);
-  radio.printDetails();
-
-  // nrf24.begin(&radio, &network, COMMS_CONTROLLER, packet_available_cb);
+  nrf24.begin(&radio, &network, COMMS_CONTROLLER, packet_available_cb);
 }
 
 void setup()
@@ -113,17 +78,17 @@ void setup()
   Serial.begin(115200);
 
   init_nrf();
-  delay(1000);
-  init_tft();
 
-  DEBUG("Ready to rx from board...");
+  delay(1000);
+
+  init_tft();
 }
 
 int i = 0;
 
 void loop()
 {
-  if (since_sent_to_board > 5000)
+  if (since_sent_to_board > 2000)
   {
     since_sent_to_board = 0;
     DEBUG("sending..");
@@ -131,16 +96,16 @@ void loop()
     tft.drawNumber(i, 20, 20);
     i++;
 
-    uint8_t bs[sizeof(VescData)];
-    memcpy(bs, &board_packet, sizeof(VescData));
+    uint8_t bs[sizeof(ControllerData)];
+    memcpy(bs, &controller_packet, sizeof(ControllerData));
 
-    // uint8_t retries = nrf24.send_with_retries(/*to*/ COMMS_BOARD, /*type*/ 0, bs, sizeof(VescData), NUM_RETRIES);
+    // uint8_t retries = nrf24.send_with_retries(/*to*/ COMMS_BOARD, /*type*/ 0, bs, sizeof(ControllerData), NUM_RETRIES);
     // if (retries > 0)
     // {
     //   DEBUGVAL(retries);
     // }
-    // board_packet.id++;
+    controller_packet.id++;
   }
-
-  // nrf24.update();
+  delay(1000);
+  nrf24.update();
 }
