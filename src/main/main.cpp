7a065dd049baf865dfe4844ea6a496ab6af7fbc4
sean-kelly <skelstar@gmail.com>
2020-03-06 13:27:58 +1300

    Redid library. Deadman funcitonality. Remapping accel/brake curves.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -51,15 +51,18 @@ RF24Network network(radio);
 //------------------------------------------------------------------
 #include <FSRThrottleLib.h>
 
+uint8_t brakePin = 34, accelPin = 35;
+
 uint8_t brakeIn[] = {0, 30, 70, 127};
 uint8_t brakeOut[] = {0, 50, 90, 127};
 FSRPin brake(/*pin*/ 35, 1800, 4095, 0, 127, brakeIn, brakeOut);
 
 uint8_t accelIn[] = {127, 180, 200, 255};
 uint8_t accelOut[] = {127, 140, 170, 255};
-FSRPin accel(/*pin*/ 34, 1800, 4095, 127, 255, accelIn, accelOut);
+FSRPin accel(/*pin*/ 34, 1800, 4095, 255, 127, accelIn, accelOut);
 
 FSRThrottleLib throttle(accel, brake);
+//------------------------------------------------------------------
 
 #define NUM_RETRIES 5
 #ifndef SEND_TO_BOARD_INTERVAL
