82c521738b0d2588ca22c420ff95a0ac6ffa2786
Sean <skelstar@gmail.com>
2020-03-07 18:01:10 +1300

    Passing in min/max raw values for FSR.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -59,11 +59,11 @@ Button2 button35(BUTTON_35);
 
 uint8_t brakeIn[] = {0, 30, 70, 127};
 uint8_t brakeOut[] = {0, 50, 90, 127};
-FSRPin brake(/*pin*/ FSR_BRAKE_PIN, 1800, 4095, 0, 127);
+FSRPin brake(/*pin*/ FSR_BRAKE_PIN, FSR_MIN_RAW, FSR_MAX_RAW, 0, 127);
 
 uint8_t accelIn[] = {127, 180, 200, 255};
 uint8_t accelOut[] = {127, 140, 170, 255};
-FSRPin accel(/*pin*/ FSR_ACCEL_PIN, 1800, 4095, 255, 127);
+FSRPin accel(/*pin*/ FSR_ACCEL_PIN, FSR_MIN_RAW, FSR_MAX_RAW, 255, 127);
 
 FSRThrottleLib throttle(&accel, &brake, &button35); //------------------------------------------------------------------
 
@@ -179,6 +179,7 @@ void setup()
 //---------------------------------------------------------------
 
 elapsedMillis since_sent_config_to_board;
+uint8_t old_throttle;
 
 void loop()
 {
@@ -187,7 +188,13 @@ void loop()
     since_read_trigger = 0;
 
     uint8_t thr = throttle.get();
-    // DEBUGVAL(thr, since_read_trigger, millis());
+#ifdef PRINT_THROTTLE
+    if (old_throttle != thr)
+    {
+      DEBUGVAL(thr);
+    }
+    old_throttle = thr;
+#endif
   }
 
   if (since_sent_to_board > SEND_TO_BOARD_INTERVAL)
