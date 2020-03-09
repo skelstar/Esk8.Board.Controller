8dff186016c5c2d38cc4651d7ddf6a50dcf8dbf9
sean-kelly <skelstar@gmail.com>
2020-03-10 08:27:24 +1300

    Imporved smoother tests and Smoother library

--- lib/Smoother/Smoother.h
+++ lib/Smoother/Smoother.h
@@ -6,43 +6,47 @@
 
 class Smoother
 {
+
 private:
-  byte _factor = 10,
+  struct Reading
+  {
+    uint8_t val;
+    bool blank;
+  };
+
+  byte _readingsLen = 10,
        _pos = 0,
        _num = 0;
-  uint8_t *_readings;
+  Reading *_readings;
 
 public:
   Smoother(byte factor, uint8_t seed, byte numSeed)
   {
-    _factor = factor;
-    _readings = new uint8_t[_factor]();
+    _readingsLen = factor;
+    _readings = new Reading[_readingsLen]();
 
-    for (int i = 0; i < _factor; i++)
-    {
-      _readings[i] = NULL;
-    }
     clear(seed, numSeed);
   }
 
   void add(uint8_t x)
   {
-    if (_num < _factor)
+    if (_num < _readingsLen)
     {
       _num++;
     }
-    _readings[_pos] = x;
-    _pos = _pos == _factor - 1 ? 0 : _pos + 1;
+    _readings[_pos].val = x;
+    _readings[_pos].blank = false;
+    _pos = _pos == _readingsLen - 1 ? 0 : _pos + 1;
   }
 
   uint8_t get()
   {
     uint16_t runningTotal = 0, runningCount = 0;
-    for (int i = 0; i < _factor; i++)
+    for (int i = 0; i < _readingsLen; i++)
     {
-      if (_readings[i] != NULL)
+      if (_readings[i].blank == false)
       {
-        runningTotal += _readings[i];
+        runningTotal += _readings[i].val;
         runningCount++;
       }
     }
@@ -53,18 +57,47 @@ public:
 
   uint8_t getLast()
   {
-    return _pos == 0 ? _readings[_factor - 1] : _readings[_pos];
+    return _pos == 0 ? _readings[_readingsLen - 1].val : _readings[_pos - 1].val;
+  }
+
+  uint16_t getIndexed(byte idx)
+  {
+    return _readings[idx].blank ? 999 : _readings[idx].val;
   }
 
   void clear(uint8_t seed, byte numSeed)
   {
-    for (int i = 0; i < _factor; i++)
+    for (int i = 0; i < _readingsLen; i++)
     {
-      _readings[i] = NULL;
+      _readings[i].blank = true;
+      _readings[i].val = 0;
     }
-    for (int j = 0; j < numSeed; j++)
+    _pos = 0;
+    // make sure numSeed is less than array size
+    for (int j = 0; j < numSeed && j < _readingsLen; j++)
     {
-      _readings[j] = seed;
+      _readings[j].val = seed;
+      _readings[j].blank = false;
+      _pos++;
     }
+  }
+
+  void printBuffer()
+  {
+    Serial.printf("[");
+    for (int i = 0; i < _readingsLen; i++)
+    {
+      if (!_readings[i].blank)
+      {
+        Serial.printf("%d", getIndexed(i));
+      }
+      else
+      {
+        Serial.printf("--");
+      }
+      if (i < _readingsLen - 1)
+        Serial.printf(", ");
+    }
+    Serial.printf("] \n");
   }
 };
--- test/test_smoother/test_smoother.cpp
+++ test/test_smoother/test_smoother.cpp
@@ -2,11 +2,11 @@
 #include <unity.h>
 #include <Smoother.h>
 
-Smoother sm_throttle(10, 127, 3);
+Smoother smoother(10, 127, 3);
 
 void setUp()
 {
-  delay(1000);
+  // delay(1000);
 }
 
 void tearDown()
@@ -15,7 +15,7 @@ void tearDown()
 
 void test_smoother_inits_properly()
 {
-  uint8_t actual = sm_throttle.get();
+  uint8_t actual = smoother.get();
   uint8_t expected = 127;
 
   TEST_ASSERT_EQUAL(expected, actual);
@@ -23,39 +23,65 @@ void test_smoother_inits_properly()
 
 void test_inits_then_adds_255_and_gets_correct_result()
 {
-  sm_throttle.add(255);
+  smoother.add(255);
 
-  uint8_t actual = sm_throttle.get();
+  uint8_t actual = smoother.get();
   uint8_t expected = (uint8_t)((127 * 3 + 255) / 4);
 
   TEST_ASSERT_EQUAL(expected, actual);
 }
 
-// void test_smoothed_throttle_is_expected()
-// {
-//   sm_throttle.(10);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
-//   sm_throttle.add(127);
+void test_when_we_add_10_items_then_one_returns_correct_result()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    smoother.add(127);
+  }
+  smoother.add(200);
+  smoother.add(127);
+  smoother.add(127);
+  smoother.add(127);
 
-//   uint8_t actual = sm_throttle.get();
-//   uint8_t expected = 127;
+  uint8_t actual = smoother.get();
+  uint8_t expected = (uint8_t)(((127 * 9) + 200) / 10);
 
-//   TEST_ASSERT_EQUAL(expected, actual);
-// }
+  TEST_ASSERT_EQUAL(expected, actual);
+}
+
+void test_when_we_add_10_items_using_clear_then_returns_correct_result()
+{
+  smoother.clear(127, 10);
+
+  uint8_t actual = smoother.get();
+  uint8_t expected = 127;
+
+  TEST_ASSERT_EQUAL(expected, actual);
+}
+
+void test_when_we_get_last_then_returns_correct_result()
+{
+  smoother.clear(0, 5);
+
+  smoother.add(1);
+  smoother.add(2);
+  smoother.add(3);
+
+  uint8_t actual = smoother.getLast();
+  uint8_t expected = 3;
+
+  smoother.printBuffer();
+
+  TEST_ASSERT_EQUAL(expected, actual);
+}
 
 void setup()
 {
   UNITY_BEGIN();
 
   RUN_TEST(test_smoother_inits_properly);
+  RUN_TEST(test_when_we_add_10_items_then_one_returns_correct_result);
+  RUN_TEST(test_when_we_add_10_items_using_clear_then_returns_correct_result);
+  RUN_TEST(test_when_we_get_last_then_returns_correct_result);
 
   UNITY_END();
 }
