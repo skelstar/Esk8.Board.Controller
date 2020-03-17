4ef29604d5e0d785d7911c084a205b5443eddfa6
Sean <skelstar@gmail.com>
2020-03-17 19:42:22 +1300

    Displaying counts on stop screen

--- include/menu_options.h
+++ include/menu_options.h
@@ -24,26 +24,40 @@ void selectOption()
     DEBUGVAL("NUM_ACCEL_COUNTS selected", optionVal);
     configStore.putUInt(STORE_CONFIG_ACCEL_COUNTS, optionVal);
     throttle.setMax(optionVal);
+    config.AccelCounts = optionVal;
     break;
   case Options::NUM_BRAKE_COUNTS:
     DEBUGVAL("NUM_BRAKE_COUNTS selected", optionVal);
     configStore.putUInt(STORE_CONFIG_BRAKE_COUNTS, optionVal);
     throttle.setMin(optionVal);
+    config.BrakeCounts = optionVal;
     break;
   }
 }
 //---------------------------------------------------------------
 void displayCurrentOption()
 {
+  char buff2[20];
+
   switch (showOption)
   {
   case Options::NONE:
     showOption = Options::NUM_ACCEL_COUNTS;
   case Options::NUM_ACCEL_COUNTS:
     DEBUG("NUM_ACCEL_COUNTS");
+    tft.fillScreen(TFT_DARKGREEN);
+    lcd_message("Accel counts", LINE_1, Aligned::ALIGNED_CENTRE);
+    sprintf(buff2, "%d", config.AccelCounts);
+    lcd_message(buff2, LINE_2, Aligned::ALIGNED_CENTRE);
+    optionVal = config.AccelCounts;
     break;
   case Options::NUM_BRAKE_COUNTS:
     DEBUG("NUM_BRAKE_COUNTS");
+    tft.fillScreen(TFT_RED);
+    lcd_message("Brake counts", LINE_1, Aligned::ALIGNED_CENTRE);
+    sprintf(buff2, "%d", config.BrakeCounts);
+    lcd_message(buff2, LINE_2, Aligned::ALIGNED_CENTRE);
+    optionVal = config.BrakeCounts;
     break;
   }
 }
