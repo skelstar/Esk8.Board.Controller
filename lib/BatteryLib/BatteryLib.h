20257bc10ca3cc6fce4402a1cd51c9bc332a278e
Sean <skelstar@gmail.com>
2020-07-06 06:28:15 +1200

    Using FastMapper

--- lib/BatteryLib/BatteryLib.h
+++ lib/BatteryLib/BatteryLib.h
@@ -2,14 +2,21 @@
 #include <Arduino.h>
 #endif
 
+#include <FastMap.h>
+
 class BatteryLib
 {
   typedef void (*BatteryValueChangedCallback)();
+
+#define REMOTE_BATTERY_FULL 2300
+#define REMOTE_BATTERY_EMPTY 1520
 
 public:
   BatteryLib(uint8_t pin)
   {
     _pin = pin;
+
+    _mapper.init(REMOTE_BATTERY_EMPTY, REMOTE_BATTERY_FULL, 0, 10);
   }
 
   void setup(BatteryValueChangedCallback cb)
@@ -39,25 +46,25 @@ public:
   uint8_t chargePercent = 0;
 
 private:
-#define REMOTE_BATTERY_FULL 2300
-#define REMOTE_BATTERY_EMPTY 1520
-
   uint8_t _get_remote_battery_percent(uint16_t raw_battery)
   {
     isCharging = raw_battery > REMOTE_BATTERY_FULL + 100;
 
-    int limited = constrain(raw_battery, REMOTE_BATTERY_EMPTY, REMOTE_BATTERY_FULL);
+    // int limited = constrain(raw_battery, REMOTE_BATTERY_EMPTY, REMOTE_BATTERY_FULL);
 
     // should return 0, 10, 20 ... 100
-    return 10 * map(limited,
-                    REMOTE_BATTERY_EMPTY,
-                    REMOTE_BATTERY_FULL,
-                    0,
-                    10);
+
+    return 10 * _mapper.constrainedMap(raw_battery);
+    //  map(limited,
+    //                 REMOTE_BATTERY_EMPTY,
+    //                 REMOTE_BATTERY_FULL,
+    //                 0,
+    //                 10);
   }
 
 private:
   BatteryValueChangedCallback _battery_value_changed_cb;
   uint8_t _pin;
   uint16_t _rawVolts;
+  FastMap _mapper;
 };
