ae16d197c55ddfbdc0256d7c2fad3a9b49fb1e61
Sean <skelstar@gmail.com>
2021-03-07 14:11:49 +1300

    Thrtl class (rel angle change solution).

--- src/mag_encoder/mag_encoder.cpp
+++ src/mag_encoder/mag_encoder.cpp
@@ -16,10 +16,135 @@ AMS_5600 ams5600;
 int ang, lang = 0;
 //---------------------------------------------------------
 
-#include <MagThrottle.h>
+// #include <MagThrottle.h>
 #include <NintendoButtons.h>
 
 //---------------------------------------------------------
+
+class Thrtl
+{
+private:
+  int16_t _throttle = 127;
+  float _centre = 0.0;
+  float _min = 0.0,
+        _max = 0.0,
+        _last = 0.0,
+        _sweep = 0.0;
+  FastMap _map;
+
+public:
+  void init(float sweep)
+  {
+    _sweep = sweep;
+    centre();
+    _map.init(0, 255, 0, 10 + 1 + 10);
+  }
+
+  float convertRawAngleToDegrees(word angle)
+  {
+    return angle * 0.087;
+  }
+
+  void centre()
+  {
+    _centre = convertRawAngleToDegrees(ams5600.getRawAngle());
+    _throttle = 127;
+    _last = _centre;
+    // _min = _centre - _sweep;
+    // if (_min < 0.0)
+    //   _min = 360.0 - abs(_min);
+    // if (_max > 360.0)
+    //   _max =
+  }
+
+  bool debugged = false;
+
+  void drawBar(int idx, char *buff, uint8_t len, uint8_t m)
+  {
+    // bottom
+    for (int j = 0; j < len; j++)
+    {
+      Serial.printf("%d,", j);
+      if (j < m)
+        buff[idx] = ' ';
+      else
+        buff[idx] = '<';
+      idx++;
+    }
+    buff[idx++] = '+';
+    // upper
+    for (int j = len + 1; j <= len + 1 + len; j++)
+    {
+      Serial.printf("%d,", j);
+      if (j > m)
+        buff[idx] = ' ';
+      else
+        buff[idx] = '>';
+      idx++;
+    }
+    buff[idx] = '\0';
+  }
+
+  uint8_t printThrottle(float delta, uint16_t throttle, char *buff)
+  {
+    if (debugged == false)
+    {
+      // Serial.printf()
+    }
+
+    int i = 0;
+    buff[i++] = delta > 0.0 ? '#' : ' ';
+    buff[i++] = delta > 10.0 ? '#' : ' ';
+    buff[i++] = delta > 20.0 ? '#' : ' ';
+    buff[i++] = delta > 30.0 ? '#' : ' ';
+    buff[i++] = delta > 40.0 ? '#' : ' ';
+    buff[i++] = delta > 50.0 ? '!' : ' ';
+
+    uint8_t m = _map.constrainedMap(throttle);
+
+    drawBar(i, buff, /*len*/ 10, m);
+
+    return m;
+  }
+
+  void update()
+  {
+    float deg = convertRawAngleToDegrees(ams5600.getRawAngle());
+    float adj = deg;
+    float delta = _last - deg;
+    bool edge = false;
+    if (abs(delta) > 180.0)
+    {
+      edge = true;
+      if (_last > deg)
+        adj += 360.0;
+      else
+        adj -= 360.0;
+      delta = _last - adj;
+    }
+    _throttle += (delta / 360.0) * 255;
+    if (_throttle > 255)
+      _throttle = 255;
+    else if (_throttle < 0)
+      _throttle = 0;
+
+    if (abs(delta) > 0.5)
+    {
+      char b[50];
+      uint8_t debug = printThrottle(abs(delta), _throttle, b);
+      Serial.printf("%s ", edge ? "EDGE" : "----");
+      // Serial.printf("| deg(adj): %05.1f ", deg);
+      // Serial.printf("| last: %05.1f", _last);
+      Serial.printf("| delta: %05.1f", delta);
+      Serial.printf("| throttle: %03d", _throttle);
+      Serial.printf("| debug: %02d", debug);
+      Serial.printf("  %s \n", b);
+    }
+
+    _last = deg;
+  }
+
+} thrtl;
 
 //---------------------------------------------------------
 
@@ -52,11 +177,13 @@ void setup()
     }
   }
 
-  MagThrottle::init(
-      /*sweep*/ 60,
-      [](uint8_t throttle) {
-        Serial.printf("   throttle: %d\n", throttle);
-      });
+  thrtl.init(30.0);
+
+  // MagThrottle::init(
+  //     /*sweep*/ 60,
+  //     [](uint8_t throttle) {
+  //       Serial.printf("   throttle: %d\n", throttle);
+  //     });
 
   delay(1000);
 }
@@ -70,7 +197,9 @@ void loop()
   {
     since_read_angle = 0;
 
-    MagThrottle::loop();
+    thrtl.update();
+
+    // MagThrottle::loop();
   }
 
   if (since_read_classic > 100)
