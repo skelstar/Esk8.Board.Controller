29efdb7f0c2d83dda864f9a7ef949a8b105bf3c6
Sean <skelstar@gmail.com>
2021-03-26 16:51:45 +1300

    Unit test added for NintendoClassicTask

--- test/tasks/tasks.cpp
+++ test/tasks/tasks.cpp
@@ -25,8 +25,10 @@ Queue::Manager *queue;
 
 #define TICKS_5 5
 #define TICKS_10 10
+#define TICKS_50 50
 
 #include <tasks/core0/qwiicButtonTask.h>
+#include <tasks/core0/NintendoClassicTask.h>
 
 #define CORE_0 0
 #define PRIORITY_1 1
@@ -43,7 +45,7 @@ void tearDown()
 {
 }
 
-void test_qwiic_queue_sent()
+void test_qwiic_button_pressed_then_released_via_queue()
 {
   Wire.begin(); //Join I2C bus
 
@@ -84,8 +86,46 @@ void test_qwiic_queue_sent()
   TEST_ASSERT_TRUE(was_pressed && was_released);
 }
 
-void test_qwiicButton()
+void test_nintendo_button_task()
 {
+  Wire.begin(); //Join I2C bus
+
+  NintendoButtonEvent *actual;
+
+  mutex_I2C.create("i2c", /*default*/ TICKS_5);
+  mutex_I2C.enabled = true;
+
+  NintendoClassicTask::createTask(CORE_0, PRIORITY_1);
+
+  while (!NintendoClassicTask::taskReady)
+  {
+    vTaskDelay(5);
+  }
+
+  Serial.printf("Press a button to satify test\n");
+
+  bool was_pressed = false, was_released = false;
+
+  while (!(was_pressed && was_released))
+  {
+    if (since_checked_queue > 200)
+    {
+      since_checked_queue = 0;
+      actual = NintendoClassicTask::queue->peek<NintendoButtonEvent>(__func__);
+      if (!was_pressed && actual != nullptr && actual->state == 1)
+      {
+        Serial.printf("Nintendo button pressed %d %d\n");
+        was_pressed = true;
+      }
+      else if (was_pressed && actual != nullptr && actual->state == 0)
+      {
+        Serial.printf("Nintendo button released\n");
+        was_released = true;
+      }
+    }
+    vTaskDelay(5);
+  }
+  TEST_ASSERT_TRUE(was_pressed && was_released);
 }
 
 void setup()
@@ -96,7 +136,8 @@ void setup()
 
   UNITY_BEGIN();
 
-  RUN_TEST(test_qwiic_queue_sent);
+  RUN_TEST(test_qwiic_button_pressed_then_released_via_queue);
+  RUN_TEST(test_nintendo_button_task);
 
   UNITY_END();
 }
