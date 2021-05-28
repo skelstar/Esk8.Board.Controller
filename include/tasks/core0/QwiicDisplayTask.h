6e76ee0088ff9d64d4943aaee62aa1c95be7eb30
Sean <skelstar@gmail.com>
2021-05-29 08:30:33 +1200

    Displaying battery volts and remote volts on QwiicDisplay

--- include/tasks/core0/QwiicDisplayTask.h
+++ include/tasks/core0/QwiicDisplayTask.h
@@ -17,11 +17,17 @@ namespace nsQwiicDisplayTask
 {
   bool m_printDebug = false;
 
+  float g_BoardBatteryVolts = 0.0;
+
   U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
   // prototypes
   void centreMessage(const char *message, bool takeMutex = true);
+  void valueMessage(const char *left, const char *right, bool takeMutex = true);
+  void initialiseQwiicDisplay();
 }
+
+#include <tasks/core0/stateMachines/QwiicDisplayStateMachine.h>
 
 class QwiicDisplayTask : public TaskBase
 {
@@ -37,6 +43,9 @@ private:
   SimplMessageObj simplMessage;
   ThrottleState _throttleState;
   Transaction m_transaction;
+
+  uint8_t _offlineCount = 0;
+  const int NUM_CONSEQ_OFFLINE_EVENTS = 3;
 
 public:
   QwiicDisplayTask() : TaskBase("QwiicDisplayTask", 3000)
@@ -56,23 +65,15 @@ public:
     simplMsgQueue = createQueueManager<SimplMessageObj>("(QwiicDisplayTask)simplMsgQueue");
     transactionQueue = createQueueManager<Transaction>("(QwiicDisplayTask)transactionQueue");
 
-    if (take(mux_I2C, TICKS_500ms))
-    {
-      Wire.begin();
-      if (u8g2.begin() == false)
-      {
-        Serial.printf("ERROR: Unable to start QwiicDisplay\n");
-      }
-      else
-      {
-        Serial.printf("Initialised QwiicDisplay OK\n");
-      }
-      // u8g2.clearBuffer();
-      // u8g2.setFont(u8g2_font_ncenR24_tn);
+    initialiseQwiicDisplay();
+    // fsm stuff
+    fsm_mgr.begin(&_fsm);
+    fsm_mgr.setPrintStateCallback(printState);
+    fsm_mgr.setPrintTriggerCallback(printTrigger);
+    addTransitions();
+    _fsm.run_machine();
 
-      give(mux_I2C);
-    }
-    centreMessage("Offline");
+    _offlineCount = 0;
   }
 
   void doWork()
@@ -82,6 +83,8 @@ public:
 
     if (transactionQueue->hasValue())
       _handleTransaction(transactionQueue->payload);
+
+    nsQwiicDisplayTask::fsm_mgr.runMachine();
   }
 
   void cleanup()
@@ -102,12 +105,16 @@ public:
 
     if (offline)
     {
+      _offlineCount++;
       // offline
-      if (wasOnline)
-        centreMessage("Offline");
+      if (_offlineCount >= NUM_CONSEQ_OFFLINE_EVENTS)
+        fsm_mgr.trigger(EV_OFFLINE);
     }
     else
     {
+      _offlineCount = 0;
+
+      g_BoardBatteryVolts = payload.batteryVolts;
       // online
       if (m_transaction.moving != payload.moving ||
           m_transaction.event_id == 0 ||
@@ -115,12 +122,12 @@ public:
       {
         if (payload.moving)
         {
-          centreMessage("Moving");
+          fsm_mgr.trigger(EV_MOVING);
         }
         else
         {
           // stopped
-          centreMessage("Stopped");
+          fsm_mgr.trigger(EV_STOPPED);
         }
       }
     }
@@ -131,6 +138,7 @@ public:
 
 QwiicDisplayTask qwiicDisplayTask;
 
+#define FONT_SIZE_MED u8g2_font_profont22_tr
 #define FONT_SIZE_LG u8g2_font_profont29_tr
 
 namespace nsQwiicDisplayTask
@@ -144,6 +152,15 @@ namespace nsQwiicDisplayTask
   {
     if (take(mux_I2C, TICKS_500ms, __func__))
     {
+      Wire.begin();
+      if (u8g2.begin() == false)
+      {
+        Serial.printf("ERROR: Unable to start QwiicDisplay\n");
+      }
+      else
+      {
+        Serial.printf("Initialised QwiicDisplay OK\n");
+      }
       give(mux_I2C);
     }
     else
@@ -168,6 +185,30 @@ namespace nsQwiicDisplayTask
     else
     {
       DEBUG("Couldn't take mutex (QwiicDisplayTask centreMessage())");
+    }
+  }
+
+  void valueMessage(const char *left, const char *right, bool takeMutex)
+  {
+    if (takeMutex || (!takeMutex && take(mux_I2C, TICKS_500ms)))
+    {
+      int w = 0, h = 0;
+
+      u8g2.clearBuffer();
+      u8g2.setFont(FONT_SIZE_MED);
+      h = u8g2.getHeight();
+      // left
+      u8g2.drawStr(0, 16 + (h / 2), left);
+
+      w = u8g2.getStrWidth(right);
+      u8g2.drawStr(128 - w, 16 + (h / 2), right);
+      u8g2.sendBuffer();
+      if (takeMutex)
+        give(mux_I2C);
+    }
+    else
+    {
+      DEBUG("Couldn't take mutex (QwiicDisplayTask valueMessage())");
     }
   }
 }
